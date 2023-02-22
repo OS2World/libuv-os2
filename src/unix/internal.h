11e98e4d25ddefd32c57a6c08d7109111bcc9c19
SilvanScherrer <silvan.scherrer@aroa.ch>
2023-02-22 22:25:54 +0100

    add a prototype for os2random

--- src/unix/internal.h
+++ src/unix/internal.h
@@ -330,6 +330,9 @@ int uv__random_getrandom(void* buf, size_t buflen);
 int uv__random_getentropy(void* buf, size_t buflen);
 int uv__random_readpath(const char* path, void* buf, size_t buflen);
 int uv__random_sysctl(void* buf, size_t buflen);
+#if defined(__OS2__)
+int uv__random_os2random(void* buf, size_t buflen);
+#endif
 
 #if defined(__APPLE__)
 int uv___stream_fd(const uv_stream_t* handle);
